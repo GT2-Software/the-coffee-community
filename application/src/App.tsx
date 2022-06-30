51859a4c00a87bd4dc4d23ec06fb2adc3e727d2b
Giovani Meza <giovanidav5@outlook.com>
2022-06-30 12:37:03 -0600

    Adds application pages
    Uses tsx componentes to render whole pages

--- application/src/App.tsx
+++ application/src/App.tsx
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Footer from './components/Footer';
+import NavigationBar from './components/NavigationBar';
+import Home from './pages/Home';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="app">
+			<div className="app-container">
+				<header className="page-header">
+					<NavigationBar />
+				</header>
+				<Home />
+				<footer className="page-footer">
+					<Footer />
+				</footer>
+			</div>
+		</div>
+	);
 }
 
 export default App;
---
+++ application/src/pages/Home.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+
+function Home() {
+	return (
+		<main className="home-page">
+			<section className="home-section">
+				<div className="home-banner">
+					<h2>Such a tiny thing, makes us so happy</h2>
+					<p className="home-paragraph">
+						Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam minima id doloremque expedita
+						fugiat neque, iusto aliquam molestias, deserunt magni ab itaque velit! Id quo, facilis magnam
+						beatae enim pariatur. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus temporibus
+						exercitationem quas voluptas, reprehenderit blanditiis distinctio cum quidem. Maiores, qui ad
+						temporibus exercitationem molestias atque reprehenderit ipsam nesciunt. Dolorum, aut. Labore,
+						adipisci excepturi perspiciatis ab eius corrupti, culpa sit dolorum debitis voluptatibus vero
+						officia amet laborum possimus neque, mollitia corporis voluptatem? Ratione quam officiis a
+						incidunt odio, numquam saepe vitae?
+					</p>
+				</div>
+				<div className="home-banner">
+					<img src={'assets/svgs/coffee1.svg'} alt="Coffee grain" className="home home-images" />
+				</div>
+			</section>
+			<section className="home-section">
+				<div className="home-banner">
+					<img src={'assets/svgs/coffee2.svg'} alt="Coffee grain" className="home home-images" />
+				</div>
+				<div className="home-banner">
+					<h2>Older as civilization</h2>
+					<p className="home-paragraph">
+						Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam minima id doloremque expedita
+						fugiat neque, iusto aliquam molestias, deserunt magni ab itaque velit! Id quo, facilis magnam
+						beatae enim pariatur. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus temporibus
+						exercitationem quas voluptas, reprehenderit blanditiis distinctio cum quidem. Maiores, qui ad
+						temporibus exercitationem molestias atque reprehenderit ipsam nesciunt. Dolorum, aut. Labore,
+						adipisci excepturi perspiciatis ab eius corrupti, culpa sit dolorum debitis voluptatibus vero
+						officia amet laborum possimus neque, mollitia corporis voluptatem? Ratione quam officiis a
+						incidunt odio, numquam saepe vitae?
+					</p>
+				</div>
+			</section>
+			<section className="home-section">
+				<div className="home-banner">
+					<h2>Olimpus beberage by excellence</h2>
+					<p className="home-paragraph">
+						Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam minima id doloremque expedita
+						fugiat neque, iusto aliquam molestias, deserunt magni ab itaque velit! Id quo, facilis magnam
+						beatae enim pariatur. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus temporibus
+						exercitationem quas voluptas, reprehenderit blanditiis distinctio cum quidem. Maiores, qui ad
+						temporibus exercitationem molestias atque reprehenderit ipsam nesciunt. Dolorum, aut. Labore,
+						adipisci excepturi perspiciatis ab eius corrupti, culpa sit dolorum debitis voluptatibus vero
+						officia amet laborum possimus neque, mollitia corporis voluptatem? Ratione quam officiis a
+						incidunt odio, numquam saepe vitae?
+					</p>
+				</div>
+				<div className="home-banner">
+					<img src={'assets/svgs/coffee3.svg'} alt="Coffee grain image" className="home home-images" />
+				</div>
+			</section>
+		</main>
+	);
+}
+
+export default Home;
